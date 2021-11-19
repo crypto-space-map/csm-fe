55ca54bdcf977e685261d6a31e2e27401bca9526
AntipovE <56717972+AntipovE@users.noreply.github.com>
2021-11-19 16:35:35 +0300

    [CSM-21] intermediate commit

--- src/app/components/csm-chart/utils/append-category-packs.ts
+++ src/app/components/csm-chart/utils/append-category-packs.ts
@@ -58,7 +58,9 @@ export const generateCategoryPacks = ({ svg, nodes, fundsTooltip }: CategoryPack
     .enter()
     .append('circle')
     .attr('fill', item => (!!item.children && item.value ? 'none' : color(item?.value / 6)))
-    .attr('stroke', item => (!!item.children ? COLOR_PALLETTE.MAIN_WHITE : '#000'))
+    .attr('stroke', item =>
+      !!item.children ? COLOR_PALLETTE.MAP_DOTTED_CIRCLES : COLOR_PALLETTE.MAP_CHILD_DASH_ARRAY
+    )
     .attr('stroke-dasharray', item => (!!item.children ? '10,10' : 'none'))
     .attr('stroke-width', 1)
     .classed(CLASSNAMES.FUND, true)
--- src/global/pallette.ts
+++ src/global/pallette.ts
@@ -3,5 +3,8 @@ export const COLOR_PALLETTE = {
   MAIN_BLUE: '#83d9f5',
   MAIN_WHITE: '#ffffff',
   ERROR_COLOR: '#FF8484',
+  // Space map colors
   MAP_LABELS: '#eae0d7',
+  MAP_DOTTED_CIRCLES: '#939393',
+  MAP_CHILD_DASH_ARRAY: '#242424',
 };
