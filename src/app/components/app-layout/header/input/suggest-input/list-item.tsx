3f6b2b3c8a803830fe776ab1db2d96a4793a9f8f
Evgeny Antipov <56717972+AntipovE@users.noreply.github.com>
2022-02-16 18:20:01 +0300

    [CSM-112] icon-plug (#43)

--- src/app/components/app-layout/header/input/suggest-input/list-item.tsx
+++ src/app/components/app-layout/header/input/suggest-input/list-item.tsx
@@ -1,10 +1,12 @@
+import { IconPlug } from 'common/components/icon-plug';
+
 import { Highlighter } from './highliter';
 import { ListItemSymbol, StyledListItem } from './styled';
 import { ListItemProps } from './types';
 
 export const ListItem = ({ children, highLight, symbol, icon, ...props }: ListItemProps) => (
   <StyledListItem {...props}>
-    <img src={icon} alt={children + symbol} />
+    {icon ? <img src={icon} alt={children + symbol} /> : <IconPlug text={children} />}
     <>
       {highLight ? <Highlighter coloredValue={highLight} item={children} i={1} /> : children}
       {symbol && <ListItemSymbol>{symbol}</ListItemSymbol>}
