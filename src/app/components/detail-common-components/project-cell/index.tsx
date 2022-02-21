94bc83a16b274adba9852eb6e905f8bba44c5425
koekto93 <koekto93@mail.ru>
2022-02-21 23:15:00 +0300

    fix: fixed Partner cell CSM-118 (#47)
    Co-authored-by: Ulybkin Mark Igorevich <ulybkin_m@utkonos.ru>

--- src/app/components/detail-common-components/project-cell/index.tsx
+++ src/app/components/detail-common-components/project-cell/index.tsx
@@ -2,6 +2,7 @@ import { useCallback } from 'react';
 
 import { CryptoLogo } from 'app/components';
 import { useDispatchAction as pageStoreDispatchAction } from 'store/pageStore/slice';
+import { addNewPath } from 'utils/router-history';
 
 import { ProjectWrapper, StyledLink, StyledText } from './styles';
 
@@ -12,11 +13,14 @@ interface InvestorsCellProps {
 }
 
 export const ProjectCell = ({ id, name, icon }: InvestorsCellProps) => {
-  const { setProjectName } = pageStoreDispatchAction();
+  const { setFundName, setProjectName } = pageStoreDispatchAction();
+  const pathName = `/project/${id}`;
 
   const handleClick = useCallback(() => {
     if (id) setProjectName(id);
-  }, [setProjectName, id]);
+    setFundName(null);
+    addNewPath(pathName);
+  }, [id, setProjectName, setFundName, pathName]);
 
   return (
     <ProjectWrapper onClick={handleClick}>
